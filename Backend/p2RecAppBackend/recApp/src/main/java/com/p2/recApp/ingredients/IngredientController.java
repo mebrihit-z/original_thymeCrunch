c1210f3761bf6ec5da58131489eb8c6b25679c1f
HannahHowell <hrhowell59@hotmail.com>
2022-01-06 16:05:29 -0600

    added arraylist for all rec method

--- Backend/p2RecAppBackend/recApp/src/main/java/com/p2/recApp/ingredients/IngredientController.java
+++ Backend/p2RecAppBackend/recApp/src/main/java/com/p2/recApp/ingredients/IngredientController.java
@@ -1,5 +1,6 @@
 package com.p2.recApp.ingredients;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +20,26 @@ import com.p2.recApp.users.UserService;
 public class IngredientController {
 	
 	private final IngredientService ingredientService;
+	private ArrayList<Recipe> recipes;
+	private Recipe pancakes;
+	private Recipe boba_tea;
+	private Recipe punch;
+	private Recipe orange_juice;
+	private Recipe latte;
+	private Recipe pavlova;
+	private Recipe black_forest_gelato_with_ganache;
+	private Recipe lemon_tart;
+	private Recipe hummus;
+	private Recipe fruit_stars;
+	private Recipe celery_boats;
+	private Recipe pad_thai;
+	private Recipe pasta;
+	private Recipe hamburger;
+	private Recipe salad;
+	private Recipe curry;
+	private Recipe bento;
+	private Recipe avocado_toast;
+	private Recipe granola;
 	@Autowired
 	public IngredientController(IngredientService ingredientService) {
 		this.ingredientService = ingredientService;
@@ -35,8 +56,36 @@ public class IngredientController {
 	}
 	
 	@GetMapping("/all-recipes")
-	public List<Ingredient> getAllRec(@PathVariable("recipe") String recipe){
-		return ingredientService.getByRec(recipe);
+	public ArrayList<Recipe> getAllRec(){
+		
+		return recipes();
+	}
+	
+	public ArrayList<Recipe> recipes(){
+		
+		recipes.add(pancakes);
+		recipes.add(granola);
+		recipes.add(avocado_toast);
+		recipes.add(bento);
+		recipes.add(curry);
+		recipes.add(salad);
+		recipes.add(hamburger);
+		recipes.add(pasta);
+		recipes.add(pad_thai);
+		recipes.add(celery_boats);
+		recipes.add(fruit_stars);
+		recipes.add(hummus);
+		recipes.add(lemon_tart);
+		recipes.add(black_forest_gelato_with_ganache);
+		recipes.add(pavlova);
+		recipes.add(latte);
+		recipes.add(orange_juice);
+		recipes.add(punch);
+		recipes.add(boba_tea);
+		
+		
+		return recipes;
+		
 	}
 	
 	//hard code this
